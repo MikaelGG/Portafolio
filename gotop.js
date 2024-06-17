d8e74ee58dad5736cb3b6599973f09e85c420bcd
MIKAELGG <mikael2g3g@gmail.com>
2024-06-17 16:29:57 -0500

    Second Commit

--- gotop.js
+++ gotop.js
@@ -12,3 +12,28 @@ document.querySelector('.go-top-container').addEventListener('click', () => {
         behavior: 'smooth'
     });
 })
+
+const modal = document.getElementById('modal');
+const modalContent = document.getElementById('modal-content');
+const urlInput = document.getElementById('urlInput');
+const modalClose = document.getElementById('modal-close');
+modalClose.addEventListener('click', function() { 
+  modal.style.display = "none";
+});
+
+function mostrarModal(img) {
+    const imgUrl = img.dataset.url;
+    const imgSrc = img.src;
+  
+    modalContent.src = imgSrc;
+    urlInput.value = imgUrl;
+    modal.style.display = "block";
+}
+
+function copyToClipboard() {
+    const input = document.querySelector('.input-box');
+    input.select();
+    document.execCommand('copy');
+    alert('Texto copiado al portapapeles');
+}
+
